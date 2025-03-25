4e4623e40e2c16b4f9527ef860f67219e753b157
outercore <gon.luna789@gmail.com>
2025-03-25 14:34:24 +0800

    for render

--- server/src/main/kotlin/org/example/project/Application.kt
+++ server/src/main/kotlin/org/example/project/Application.kt
@@ -48,7 +48,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.mindrot.jbcrypt.BCrypt
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "192.168.8.132", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
